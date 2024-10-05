16a5309ef42c8d241cba98bc52a38d73f80476fd
ranaufalmuha <ifal.muha@gmail.com>
2024-10-05 10:53:43 +0700

    add smart contract

--- anchor/programs/linkthree/src/lib.rs
+++ anchor/programs/linkthree/src/lib.rs
@@ -8,63 +8,179 @@ declare_id!("CWPtWybP2yNXgPHWnzSuCjyoTaXqzbcGu52THbNdvV6t");
 pub mod linkthree {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseLinkthree>) -> Result<()> {
-    Ok(())
-  }
+    // Close the Linkthree account
+    pub fn close(_ctx: Context<CloseLinkthree>) -> Result<()> {
+        Ok(())
+    }
 
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.linkthree.count = ctx.accounts.linkthree.count.checked_sub(1).unwrap();
-    Ok(())
-  }
+    // Decrement the count value
+    pub fn decrement(ctx: Context<Update>) -> Result<()> {
+        ctx.accounts.linkthree.count = ctx.accounts.linkthree.count.checked_sub(1).unwrap();
+        Ok(())
+    }
 
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.linkthree.count = ctx.accounts.linkthree.count.checked_add(1).unwrap();
-    Ok(())
-  }
+    // Increment the count value
+    pub fn increment(ctx: Context<Update>) -> Result<()> {
+        ctx.accounts.linkthree.count = ctx.accounts.linkthree.count.checked_add(1).unwrap();
+        Ok(())
+    }
 
-  pub fn initialize(_ctx: Context<InitializeLinkthree>) -> Result<()> {
-    Ok(())
-  }
+    // Set the count value
+    pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
+        ctx.accounts.linkthree.count = value;
+        Ok(())
+    }
 
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.linkthree.count = value.clone();
-    Ok(())
-  }
+    // Initialize the Linkthree account
+    pub fn initialize(
+        ctx: Context<InitializeLinkthree>,
+        owner: Pubkey,
+        name: String,
+        photo_profile: String,
+        position: String,
+        description: String,
+    ) -> Result<()> {
+        let linkthree = &mut ctx.accounts.linkthree;
+        linkthree.count = 0;
+        linkthree.owner = owner;
+        linkthree.name = name;
+        linkthree.photo_profile = photo_profile;
+        linkthree.position = position;
+        linkthree.description = description;
+        Ok(())
+    }
+
+    // Function to update all attributes at once
+    pub fn update_all(
+        ctx: Context<Update>,
+        new_name: String,
+        new_photo_profile: String,
+        new_position: String,
+        new_description: String,
+    ) -> Result<()> {
+        let linkthree = &mut ctx.accounts.linkthree;
+        linkthree.name = new_name;
+        linkthree.photo_profile = new_photo_profile;
+        linkthree.position = new_position;
+        linkthree.description = new_description;
+        Ok(())
+    }
+
+    // Add or update a social entry in the array
+    pub fn upsert_social(ctx: Context<Update>, index: u8, icon: String, url: String) -> Result<()> {
+        let linkthree = &mut ctx.accounts.linkthree;
+        if (index as usize) < linkthree.social.len() {
+            linkthree.social[index as usize] = Social { icon, url };
+        }
+        Ok(())
+    }
+
+    // Add or update a link entry in the array
+    pub fn upsert_link(
+        ctx: Context<Update>,
+        index: u8,
+        image_background: String,
+        url: String,
+    ) -> Result<()> {
+        let linkthree = &mut ctx.accounts.linkthree;
+        if (index as usize) < linkthree.links.len() {
+            linkthree.links[index as usize] = Link {
+                image_background,
+                url,
+            };
+        }
+        Ok(())
+    }
+
+    // Delete a social entry by index
+    pub fn delete_social(ctx: Context<Update>, index: u8) -> Result<()> {
+        let linkthree = &mut ctx.accounts.linkthree;
+        if (index as usize) < linkthree.social.len() {
+            linkthree.social[index as usize] = Social {
+                icon: String::new(),
+                url: String::new(),
+            };
+        }
+        Ok(())
+    }
+
+    // Delete a link entry by index
+    pub fn delete_link(ctx: Context<Update>, index: u8) -> Result<()> {
+        let linkthree = &mut ctx.accounts.linkthree;
+        if (index as usize) < linkthree.links.len() {
+            linkthree.links[index as usize] = Link {
+                image_background: String::new(),
+                url: String::new(),
+            };
+        }
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct InitializeLinkthree<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
 
-  #[account(
-  init,
-  space = 8 + Linkthree::INIT_SPACE,
-  payer = payer
-  )]
-  pub linkthree: Account<'info, Linkthree>,
-  pub system_program: Program<'info, System>,
+    #[account(
+        init,
+        space = 8 + Linkthree::INIT_SPACE,
+        payer = payer
+    )]
+    pub linkthree: Account<'info, Linkthree>,
+    pub system_program: Program<'info, System>,
 }
+
 #[derive(Accounts)]
 pub struct CloseLinkthree<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
 
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub linkthree: Account<'info, Linkthree>,
+    #[account(
+        mut,
+        close = payer, // Close account and return lamports to payer
+    )]
+    pub linkthree: Account<'info, Linkthree>,
 }
 
 #[derive(Accounts)]
 pub struct Update<'info> {
-  #[account(mut)]
-  pub linkthree: Account<'info, Linkthree>,
+    #[account(mut)]
+    pub linkthree: Account<'info, Linkthree>,
 }
 
 #[account]
 #[derive(InitSpace)]
 pub struct Linkthree {
-  count: u8,
+    count: u8,
+
+    pub owner: Pubkey,
+    #[max_len(20)]
+    pub name: String,
+    #[max_len(500)]
+    pub photo_profile: String,
+    #[max_len(50)]
+    pub position: String,
+    #[max_len(300)]
+    pub description: String,
+    pub social: [Social; 100],
+    pub links: [Link; 100],
+}
+
+#[derive(Clone, AnchorSerialize, AnchorDeserialize, InitSpace)]
+pub struct Social {
+    // count: u8,
+    #[max_len(50)]
+    pub icon: String,
+    #[max_len(100)]
+    pub url: String,
+}
+
+#[derive(Clone, AnchorSerialize, AnchorDeserialize, InitSpace)]
+pub struct Link {
+    // count: u8,
+    #[max_len(500)]
+    pub image_background: String,
+    #[max_len(500)]
+    pub url: String,
 }
--- anchor/target/types/linkthree.ts
+++ anchor/target/types/linkthree.ts
@@ -5,108 +5,414 @@
  * IDL can be found at `target/idl/linkthree.json`.
  */
 export type Linkthree = {
-  address: 'CWPtWybP2yNXgPHWnzSuCjyoTaXqzbcGu52THbNdvV6t';
-  metadata: {
-    name: 'linkthree';
-    version: '0.1.0';
-    spec: '0.1.0';
-    description: 'Created with Anchor';
-  };
-  instructions: [
+  "address": "CWPtWybP2yNXgPHWnzSuCjyoTaXqzbcGu52THbNdvV6t",
+  "metadata": {
+    "name": "linkthree",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
     {
-      name: 'close';
-      discriminator: [98, 165, 201, 177, 108, 65, 206, 96];
-      accounts: [
+      "name": "close",
+      "discriminator": [
+        98,
+        165,
+        201,
+        177,
+        108,
+        65,
+        206,
+        96
+      ],
+      "accounts": [
         {
-          name: 'payer';
-          writable: true;
-          signer: true;
+          "name": "payer",
+          "writable": true,
+          "signer": true
         },
         {
-          name: 'linkthree';
-          writable: true;
+          "name": "linkthree",
+          "writable": true
         }
-      ];
-      args: [];
+      ],
+      "args": []
     },
     {
-      name: 'decrement';
-      discriminator: [106, 227, 168, 59, 248, 27, 150, 101];
-      accounts: [
+      "name": "decrement",
+      "discriminator": [
+        106,
+        227,
+        168,
+        59,
+        248,
+        27,
+        150,
+        101
+      ],
+      "accounts": [
         {
-          name: 'linkthree';
-          writable: true;
+          "name": "linkthree",
+          "writable": true
         }
-      ];
-      args: [];
+      ],
+      "args": []
     },
     {
-      name: 'increment';
-      discriminator: [11, 18, 104, 9, 104, 174, 59, 33];
-      accounts: [
+      "name": "deleteLink",
+      "discriminator": [
+        160,
+        191,
+        177,
+        176,
+        99,
+        40,
+        106,
+        98
+      ],
+      "accounts": [
         {
-          name: 'linkthree';
-          writable: true;
+          "name": "linkthree",
+          "writable": true
         }
-      ];
-      args: [];
+      ],
+      "args": [
+        {
+          "name": "index",
+          "type": "u8"
+        }
+      ]
     },
     {
-      name: 'initialize';
-      discriminator: [175, 175, 109, 31, 13, 152, 155, 237];
-      accounts: [
+      "name": "deleteSocial",
+      "discriminator": [
+        62,
+        241,
+        46,
+        70,
+        69,
+        6,
+        38,
+        177
+      ],
+      "accounts": [
         {
-          name: 'payer';
-          writable: true;
-          signer: true;
+          "name": "linkthree",
+          "writable": true
+        }
+      ],
+      "args": [
+        {
+          "name": "index",
+          "type": "u8"
+        }
+      ]
+    },
+    {
+      "name": "increment",
+      "discriminator": [
+        11,
+        18,
+        104,
+        9,
+        104,
+        174,
+        59,
+        33
+      ],
+      "accounts": [
+        {
+          "name": "linkthree",
+          "writable": true
+        }
+      ],
+      "args": []
+    },
+    {
+      "name": "initialize",
+      "discriminator": [
+        175,
+        175,
+        109,
+        31,
+        13,
+        152,
+        155,
+        237
+      ],
+      "accounts": [
+        {
+          "name": "payer",
+          "writable": true,
+          "signer": true
         },
         {
-          name: 'linkthree';
-          writable: true;
-          signer: true;
+          "name": "linkthree",
+          "writable": true,
+          "signer": true
         },
         {
-          name: 'systemProgram';
-          address: '11111111111111111111111111111111';
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
         }
-      ];
-      args: [];
+      ],
+      "args": [
+        {
+          "name": "owner",
+          "type": "pubkey"
+        },
+        {
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "name": "photoProfile",
+          "type": "string"
+        },
+        {
+          "name": "position",
+          "type": "string"
+        },
+        {
+          "name": "description",
+          "type": "string"
+        }
+      ]
     },
     {
-      name: 'set';
-      discriminator: [198, 51, 53, 241, 116, 29, 126, 194];
-      accounts: [
+      "name": "set",
+      "discriminator": [
+        198,
+        51,
+        53,
+        241,
+        116,
+        29,
+        126,
+        194
+      ],
+      "accounts": [
         {
-          name: 'linkthree';
-          writable: true;
+          "name": "linkthree",
+          "writable": true
         }
-      ];
-      args: [
+      ],
+      "args": [
         {
-          name: 'value';
-          type: 'u8';
+          "name": "value",
+          "type": "u8"
         }
-      ];
+      ]
+    },
+    {
+      "name": "updateAll",
+      "discriminator": [
+        205,
+        139,
+        239,
+        66,
+        134,
+        131,
+        110,
+        182
+      ],
+      "accounts": [
+        {
+          "name": "linkthree",
+          "writable": true
+        }
+      ],
+      "args": [
+        {
+          "name": "newName",
+          "type": "string"
+        },
+        {
+          "name": "newPhotoProfile",
+          "type": "string"
+        },
+        {
+          "name": "newPosition",
+          "type": "string"
+        },
+        {
+          "name": "newDescription",
+          "type": "string"
+        }
+      ]
+    },
+    {
+      "name": "upsertLink",
+      "discriminator": [
+        156,
+        211,
+        176,
+        98,
+        92,
+        59,
+        64,
+        118
+      ],
+      "accounts": [
+        {
+          "name": "linkthree",
+          "writable": true
+        }
+      ],
+      "args": [
+        {
+          "name": "index",
+          "type": "u8"
+        },
+        {
+          "name": "imageBackground",
+          "type": "string"
+        },
+        {
+          "name": "url",
+          "type": "string"
+        }
+      ]
+    },
+    {
+      "name": "upsertSocial",
+      "discriminator": [
+        69,
+        245,
+        33,
+        88,
+        44,
+        124,
+        94,
+        9
+      ],
+      "accounts": [
+        {
+          "name": "linkthree",
+          "writable": true
+        }
+      ],
+      "args": [
+        {
+          "name": "index",
+          "type": "u8"
+        },
+        {
+          "name": "icon",
+          "type": "string"
+        },
+        {
+          "name": "url",
+          "type": "string"
+        }
+      ]
     }
-  ];
-  accounts: [
+  ],
+  "accounts": [
     {
-      name: 'linkthree';
-      discriminator: [135, 64, 223, 168, 233, 143, 162, 215];
+      "name": "linkthree",
+      "discriminator": [
+        198,
+        40,
+        192,
+        150,
+        193,
+        103,
+        17,
+        37
+      ]
     }
-  ];
-  types: [
+  ],
+  "types": [
     {
-      name: 'linkthree';
-      type: {
-        kind: 'struct';
-        fields: [
+      "name": "link",
+      "type": {
+        "kind": "struct",
+        "fields": [
           {
-            name: 'count';
-            type: 'u8';
+            "name": "imageBackground",
+            "type": "string"
+          },
+          {
+            "name": "url",
+            "type": "string"
           }
-        ];
-      };
+        ]
+      }
+    },
+    {
+      "name": "linkthree",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "count",
+            "type": "u8"
+          },
+          {
+            "name": "owner",
+            "type": "pubkey"
+          },
+          {
+            "name": "name",
+            "type": "string"
+          },
+          {
+            "name": "photoProfile",
+            "type": "string"
+          },
+          {
+            "name": "position",
+            "type": "string"
+          },
+          {
+            "name": "description",
+            "type": "string"
+          },
+          {
+            "name": "social",
+            "type": {
+              "array": [
+                {
+                  "defined": {
+                    "name": "social"
+                  }
+                },
+                100
+              ]
+            }
+          },
+          {
+            "name": "links",
+            "type": {
+              "array": [
+                {
+                  "defined": {
+                    "name": "link"
+                  }
+                },
+                100
+              ]
+            }
+          }
+        ]
+      }
+    },
+    {
+      "name": "social",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "icon",
+            "type": "string"
+          },
+          {
+            "name": "url",
+            "type": "string"
+          }
+        ]
+      }
     }
-  ];
+  ]
 };
